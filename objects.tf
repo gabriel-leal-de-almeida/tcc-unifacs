03ce690a559b971dca685ad503a556357da1ba07
Gabriel Leal de Almeida <112120035+gabriel-leal-de-almeida@users.noreply.github.com>
2024-10-13 17:05:57 -0300

    Revert "refactor: Update Terraform objects.tf to use variables for script names"
    This reverts commit a20f3ec220770e3c559f3f415595c7dc456cac20.

--- objects.tf
+++ objects.tf
@@ -1,43 +1,37 @@
 # Carrega os scripts para o bucket do Google Cloud Storage
 resource "google_storage_bucket_object" "process_data_dataproc_script" {
-  name   = "process_data.py"
-  output_name = "${var.dataproc_scripts_folder}process_data.py"
+  name   = "${var.dataproc_scripts_folder}process_data.py"
   bucket = var.bucket_name
   source = "${path.module}/${var.dataproc_scripts_folder}/process_data.py"
 }
 
 resource "google_storage_bucket_object" "read_data_dataproc_script" {
-  name   = "read_data.py"
-  output_name = "${var.dataproc_scripts_folder}read_data.py"
+  name   = "${var.dataproc_scripts_folder}read_data.py"
   bucket = var.bucket_name
   source = "${path.module}/${var.dataproc_scripts_folder}/read_data.py"
 }
 
 resource "google_storage_bucket_object" "collect_metrics_functions_script" {
-  name   = "collect_metrics.py"
-  output_name = "${var.functions_scripts_folder}collect_metrics.py"
+  name   = "${var.functions_scripts_folder}collect_metrics.py"
   bucket = var.bucket_name
   source = "${path.module}/${var.functions_scripts_folder}/collect_metrics.py"
 }
 
 # Upload scripts to GCS
 resource "google_storage_bucket_object" "start_process_data_functions_script" {
-  name   = "start_process_data.py"
-  output_name = "${var.functions_scripts_folder}start_process_data.py"
+  name   = "${var.functions_scripts_folder}start_process_data.py"
   bucket = var.bucket_name
   source = "${path.module}/${var.functions_scripts_folder}/start_process_data.py"
 }
 
 resource "google_storage_bucket_object" "start_read_data_functions_script" {
-  name   = "start_read_data.py"
-  output_name = "${var.functions_scripts_folder}start_read_data.py"
+  name   = "${var.functions_scripts_folder}start_read_data.py"
   bucket = var.bucket_name
   source = "${path.module}/${var.functions_scripts_folder}/start_read_data.py"
 }
 
 resource "google_storage_bucket_object" "start_collect_metrics_functions_script" {
-  name   = "start_collect_metrics.py"
-  output_name = "${var.functions_scripts_folder}start_collect_metrics.py"
+  name   = "${var.functions_scripts_folder}start_collect_metrics.py"
   bucket = var.bucket_name
   source = "${path.module}/${var.functions_scripts_folder}/start_collect_metrics.py"
 }
