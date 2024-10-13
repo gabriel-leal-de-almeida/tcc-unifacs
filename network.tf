93079c52d300611f8566538caeca1b463898428f
Gabriel Leal de Almeida <112120035+gabriel-leal-de-almeida@users.noreply.github.com>
2024-10-13 20:37:58 -0300

    Remove Google Compute subnetwork resource

--- network.tf
+++
@@ -1,8 +0,0 @@
-resource "google_compute_subnetwork" "default" {
-  name          = "default"
-  ip_cidr_range = "10.142.0.0/20"
-  region        = var.region
-  network       = "default"
-
-  private_ip_google_access = true
-}
