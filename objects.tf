9a1e55bd4656a827d3c2086e879f1c01059e0aaa
Gabriel Leal de Almeida <112120035+gabriel-leal-de-almeida@users.noreply.github.com>
2024-10-16 17:36:30 -0300

    Refactor script to add Cloud Function for writing BigQuery results

--- objects.tf
+++ objects.tf
@@ -57,6 +57,12 @@ resource "google_storage_bucket_object" "start_collect_metrics_functions_script"
   depends_on = [data.archive_file.start_collect_metrics_functions_script_zip]
 }
 
+data "archive_file" "start_write_bigquery_functions_script_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/${var.functions_scripts_folder}/start_write_bigquery"
+  output_path = "${path.module}/zips/start_write_bigquery.zip"
+}
+
 resource "google_storage_bucket_object" "start_write_bigquery_functions_script" {
   name   = "${var.functions_scripts_folder}start_write_bigquery.zip"
   bucket = var.bucket_name
