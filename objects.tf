9732c56d3b5f7827eba7f6776515eb78772b3037
Gabriel Leal de Almeida <112120035+gabriel-leal-de-almeida@users.noreply.github.com>
2024-10-27 15:50:20 -0300

    Refactor objects.tf to update the resource name and source path for the Spark Avro JAR file

--- objects.tf
+++ objects.tf
@@ -70,18 +70,17 @@ resource "google_storage_bucket_object" "start_write_bigquery_functions_script" 
   depends_on = [data.archive_file.start_write_bigquery_functions_script_zip]
 }
 
-# Baixe o JAR do Maven usando um null_resource
-resource "null_resource" "download_spark_avro_jar" {
-  provisioner "local-exec" {
-    command = "curl -L -o /tmp/spark-avro_2.13-3.5.1.jar https://repo1.maven.org/maven2/org/apache/spark/spark-avro_2.13/3.5.1/spark-avro_2.13-3.5.1.jar"
-  }
+# Baixe o JAR do Maven usando um recurso local_file
+resource "local_file" "download_spark_avro_jar" {
+  filename = "${path.module}/tmp/spark-avro_2.13-3.5.1.jar"
+  content  = filebase64("https://repo1.maven.org/maven2/org/apache/spark/spark-avro_2.13/3.5.1/spark-avro_2.13-3.5.1.jar")
 }
 
 # Carregue o JAR para o bucket do GCS
 resource "google_storage_bucket_object" "spark_avro_jar" {
   name   = "libs/jars/spark-avro_2.13-3.5.1.jar"
   bucket = var.bucket_name
-  source = "/tmp/spark-avro_2.13-3.5.1.jar"
+  source = local_file.download_spark_avro_jar.filename
 
-  depends_on = [null_resource.download_spark_avro_jar]
+  depends_on = [local_file.download_spark_avro_jar]
 }
