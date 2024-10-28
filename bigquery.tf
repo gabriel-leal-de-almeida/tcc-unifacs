6b0bf5fb688386774b5e27fc799aa46288f770e3
Gabriel Leal de Almeida <112120035+gabriel-leal-de-almeida@users.noreply.github.com>
2024-10-28 12:13:31 -0300

    Refatorar esquema da tabela BigQuery para usar aspas duplas em referências de tabela

--- bigquery.tf
+++ bigquery.tf
@@ -277,7 +277,7 @@ resource "google_bigquery_table" "complete_metrics" {
     deletion_protection = true
 
     view {
-        query = <<-SQL
+        query = <<-EOF
             SELECT
                 process_data_metrics.spark_version AS process_data_spark_version,
                 process_data_metrics.python_version AS process_data_python_version,
@@ -318,10 +318,10 @@ resource "google_bigquery_table" "complete_metrics" {
                 read_data_metrics.job_end_time AS read_data_job_end_time,
                 read_data_metrics.total_duration_sec AS read_data_total_duration_sec
             FROM
-                ${var.project_id}.${google_bigquery_dataset.metrics.dataset_id}.process_data_metrics AS process_data_metrics
+                `${var.project_id}.metrics.process_data_metrics` AS process_data_metrics
             JOIN
-                ${var.project_id}.${google_bigquery_dataset.metrics.dataset_id}.read_data_metrics AS read_data_metrics ON
+                `${var.project_id}.metrics.read_data_metrics` AS read_data_metrics ON
                     process_data_metrics.execution_id = read_data_metrics.execution_id
-        SQL
+        EOF
     }
 }
