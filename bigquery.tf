b67ff159330a7c96ae198b9e2ce4f53100bfdf9c
Gabriel Leal de Almeida <112120035+gabriel-leal-de-almeida@users.noreply.github.com>
2024-10-27 23:45:41 -0300

    Refactor BigQuery table schema to disable deletion protection

--- bigquery.tf
+++ bigquery.tf
@@ -133,4 +133,138 @@ resource "google_bigquery_table" "process_data_metrics" {
     }
 }
 
-    
+resource "google_bigquery_table" "read_data_metrics" {
+    dataset_id = google_bigquery_dataset.metrics.dataset_id
+    table_id = "read_data_metrics"
+    project = var.project_id
+    deletion_protection = false
+
+    schema = jsonencode([
+        {
+            name = "spark_version"
+            type = "STRING"
+            mode = "REQUIRED"
+            description = "A versão do Spark utilizada para processar os dados."
+        },
+        {
+            name = "python_version"
+            type = "STRING"
+            mode = "REQUIRED"
+            description = "A versão do Python utilizada para processar os dados."
+        },
+        {
+            name = "execution_id"
+            type = "STRING"
+            mode = "REQUIRED"
+            description = "O ID da execução do job."
+        },
+        {
+            name = "description"
+            type = "STRING"
+            mode = "NULLABLE"
+            description = "A descrição do job."
+        },
+        {
+            name = "format"
+            type = "STRING"
+            mode = "REQUIRED"
+            description = "O formato de leitura dos dados."
+        },
+        {
+            name = "input_path"
+            type = "STRING"
+            mode = "REQUIRED"
+            description = "O caminho de leitura dos dados."
+        },
+        {
+            name = "read_duration_sec"
+            type = "FLOAT"
+            mode = "NULLABLE"
+            description = "O tempo de leitura dos dados em segundos."
+        },
+        {
+            name = "count_duration_sec"
+            type = "FLOAT"
+            mode = "NULLABLE"
+            description = "O tempo de contagem dos registros em segundos."
+        },
+        {
+            name = "record_count"
+            type = "INTEGER"
+            mode = "NULLABLE"
+            description = "O número total de registros lidos."
+        },
+        {
+            name = "distinct_count"
+            type = "INTEGER"
+            mode = "NULLABLE"
+            description = "O número total de registros distintos."
+        },
+        {
+            name = "aggregation_duration_sec"
+            type = "FLOAT"
+            mode = "NULLABLE"
+            description = "O tempo de agregação dos dados em segundos."
+        },
+        {
+            name = "filtered_count"
+            type = "INTEGER"
+            mode = "NULLABLE"
+            description = "O número de registros filtrados."
+        },
+        {
+            name = "filter_duration_sec"
+            type = "FLOAT"
+            mode = "NULLABLE"
+            description = "O tempo de filtragem dos dados em segundos."
+        },
+        {
+            name = "sort_duration_sec"
+            type = "FLOAT"
+            mode = "NULLABLE"
+            description = "O tempo de ordenação dos dados em segundos."
+        },
+        {
+            name = "select_columns_duration_sec"
+            type = "FLOAT"
+            mode = "NULLABLE"
+            description = "O tempo de seleção das colunas em segundos."
+        },
+        {
+            name = "time_range_filter_duration_sec"
+            type = "FLOAT"
+            mode = "NULLABLE"
+            description = "O tempo de filtragem por intervalo de tempo em segundos."
+        },
+        {
+            name = "numeric_filter_duration_sec"
+            type = "FLOAT"
+            mode = "NULLABLE"
+            description = "O tempo de filtragem numérica em segundos."
+        },
+        {
+            name = "job_start_time"
+            type = "TIMESTAMP"
+            mode = "REQUIRED"
+            description = "O horário de início do job."
+        },
+        {
+            name = "job_end_time"
+            type = "TIMESTAMP"
+            mode = "REQUIRED"
+            description = "O horário de término do job em segundos."
+        },
+        {
+            name = "total_duration_sec"
+            type = "FLOAT"
+            mode = "NULLABLE"
+            description = "O tempo total de execução do job em segundos."
+        }
+    ])
+
+    external_data_configuration {
+        autodetect = false
+        source_format = "NEWLINE_DELIMITED_JSON"
+        source_uris = ["gs://${var.bucket_name}/metrics/read-data/*.json"]
+    }
+}
